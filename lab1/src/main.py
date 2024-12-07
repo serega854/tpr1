dee68a55da77af927565f24a1b4fd8a388c454c6
Serega854 <novruzovsergey2002@yandex.ru>
2024-12-07 10:04:02 +0300

    убрал один пробел

--- lab1/src/main.py
+++ lab1/src/main.py
@@ -14,7 +14,7 @@ def get_path_from_arguments(args) -> str:
 
 
 def main():
-    path = get_path_from_arguments(sys.argv[1:])   
+    path = get_path_from_arguments(sys.argv[1:])
     reader = TextDataReader()
     students = reader.read(path)
     print("Students: ", students)
